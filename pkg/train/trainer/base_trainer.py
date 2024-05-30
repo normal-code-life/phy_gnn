3f0f17567676aae6ae2a8da279f4fb0a22b65bc4
code-life <code-life>
2024-05-30 23:54:44 +0800

    graph sage 5

--- pkg/train/trainer/base_trainer.py
+++ pkg/train/trainer/base_trainer.py
@@ -23,7 +23,7 @@ from pkg.utils.model_summary import summary
 logger = init_logger("BASE_TRAINER")
 
 
-torch.cuda.set_device("cuda:1")
+# torch.cuda.set_device("cuda:1")
 
 class TrainerConfig(BaseConfig):
     """TrainerConfig class is inherent from BaseConfig class defining the structure for Trainer configuration."""
